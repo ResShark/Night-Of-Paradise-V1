b5830c096d472744bc6665674d987d0c3a9f08a1
ResShark <62224605+ResShark@users.noreply.github.com>
2020-09-18 14:29:37 +0200

    Add files via upload

---
+++ [ESX]/[non-esx]/Cross-Loading-Screen/README.md
@@ -0,0 +1,5 @@
+Discord : https://discord.gg/J5HFpkF
+
+![fivem](https://i.imgur.com/ZIpTIcQ.png)
+
+Vidéo : https://youtu.be/tU7TFZq9w-c
---
+++ [ESX]/[non-esx]/Cross-Loading-Screen/__resource.lua
@@ -0,0 +1,10 @@
+files {
+    'index.html',
+    'style.css',
+    'img/background.jpg',
+    'music/loading.ogg'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
