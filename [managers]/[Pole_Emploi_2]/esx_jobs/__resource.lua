9f2a16142400b90755a68b38177206c9ca65f840
ResShark <62224605+ResShark@users.noreply.github.com>
2020-09-18 14:44:45 +0200

    Add files via upload

---
+++ [managers]/[Pole_Emploi_2]/esx_jobs/__resource.lua
@@ -0,0 +1,33 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Jobs'
+
+version '1.1.0'
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'client/jobs/fisherman.lua',
+	'client/jobs/fueler.lua',
+	'client/jobs/lumberjack.lua',
+	'client/jobs/miner.lua',
+	'client/jobs/reporter.lua',
+	'client/jobs/slaughterer.lua',
+	'client/jobs/tailor.lua',
+	'client/main.lua'
+}
+
+dependencies {
+	'es_extended',
+	'esx_addonaccount',
+	'skinchanger',
+	'esx_skin'
+}
