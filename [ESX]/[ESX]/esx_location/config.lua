21712a165e3ceaf99694b71d4d04ea00a5dac952
ResShark <62224605+ResShark@users.noreply.github.com>
2020-09-18 14:10:11 +0200

    Add files via upload

---
+++ [ESX]/[ESX]/esx_location/config.lua
@@ -0,0 +1,13 @@
+Config                            = {}
+Config.DrawDistance               = 150.0
+
+Config.Zones = {
+
+    LocationVehicleEntering = {
+        Pos = {x = -1034.68, y = -2733.85, z = 19.16}, -- Position location de Véhicule
+        Size = { x = 1.5, y = 1.5, z= 0.5},
+        Color = {r = 209, g = 255, b = 0},
+        Type = 1,
+    },
+
+}
