1496923af07ae10c67125a4b2afb3ed91eee748c
ResShark <62224605+ResShark@users.noreply.github.com>
2020-09-18 14:50:31 +0200

    Add files via upload

---
+++ [mapping]/LegionSquare/__resource.lua
@@ -0,0 +1,5 @@
+this_is_a_map "yes"
+
+name "Legion Square Upgrade."
+author "Mart475 - Universal Development."
+description "The Legion Square Upgrade is an optimized and basic upgrade of the legion square area."
